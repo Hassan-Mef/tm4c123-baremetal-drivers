01926141a8a935158798be1b6fe385643d448555
hassan-mef <hassanmef19@gmail.com>
2026-07-15 11:13:20 +0500

    Remove commented-out code for clarity

--- src/main.c
+++ src/main.c
@@ -60,34 +60,6 @@ static void greenCallback(void)
 
 int main(void)
 {
-
-     // gpio_configType redLed =
-    // {
-    //     .port = GPIO_PORT_F,
-    //     .pin = GPIO_PIN_1,
-    //     .mode = GPIO_MODE_OUTPUT
-    // };
-
-    // gpio_configType blueLed =
-    // {
-    //     .port = GPIO_PORT_F,
-    //     .pin = GPIO_PIN_2,
-    //     .mode = GPIO_MODE_OUTPUT
-    // };
-
-    // /* Initialize LEDs */
-    // gpio_init(&redLed);
-    // gpio_init(&blueLed);
-
-    // while (1)
-    // {
-    //     gpio_digitalToggle(&redLed);
-    //     delay();
-
-    //     gpio_digitalToggle(&blueLed);
-    //     delay();
-    // }
-
     /* Initialize GPIO pins for LEDs */
     gpio_init(&redLed);
     gpio_init(&blueLed);
@@ -96,116 +68,102 @@ int main(void)
 
     /* Set up timer configuration */
 
-timer_configType timer0 =
-{
-    .number     = TIMER_0,
-    .channel    = TIMER_AB,
-    .mode       = TIMER_MODE_PERIODIC,
-    .direction  = TIMER_COUNT_DOWN,
-    .size       = TIMER_SIZE_32_BIT,
-    .prescaler  = 0U,
-    .unit       = TIMER_MS,
-    .interrupt  = TIMER_INTERRUPT_ENABLE
-};
-
-timer_configType timer1 =
-{
-    .number     = TIMER_1,
-    .channel    = TIMER_AB,
-    .mode       = TIMER_MODE_PERIODIC,
-    .direction  = TIMER_COUNT_DOWN,
-    .size       = TIMER_SIZE_32_BIT,
-    .prescaler  = 0U,
-    .unit       = TIMER_MS,
-    .interrupt  = TIMER_INTERRUPT_ENABLE
-};
-
-timer_configType timer2 =
-{
-    .number     = TIMER_2,
-    .channel    = TIMER_AB,
-    .mode       = TIMER_MODE_PERIODIC,
-    .direction  = TIMER_COUNT_DOWN,
-    .size       = TIMER_SIZE_32_BIT,
-    .prescaler  = 0U,
-    .unit       = TIMER_MS,
-    .interrupt  = TIMER_INTERRUPT_ENABLE
-};
-
-timer_configType timer3 =
-{
-    .number     = TIMER_3,
-    .channel    = TIMER_AB,
-    .mode       = TIMER_MODE_ONE_SHOT,
-    .direction  = TIMER_COUNT_DOWN,
-    .size       = TIMER_SIZE_32_BIT,
-    .prescaler  = 0U,
-    .unit       = TIMER_MS,
-    .interrupt  = TIMER_INTERRUPT_ENABLE
-};
-timer_configType timer4 =
-{
-    .number     = TIMER_4,
-    .channel    = TIMER_AB,
-    .mode       = TIMER_MODE_ONE_SHOT,
-    .direction  = TIMER_COUNT_DOWN,
-    .size       = TIMER_SIZE_32_BIT,
-    .prescaler  = 0U,
-    .unit       = TIMER_MS,
-    .interrupt  = TIMER_INTERRUPT_DISABLE
-};
-
-timer_init(&timer4);
-
-/* Initialize timers and set callbacks */
-timer_init(&timer0);
-timer_init(&timer1);
-timer_init(&timer2);
-timer_init(&timer3);
-
-timer_setCallback(TIMER_0, redCallback);
-timer_setCallback(TIMER_1, blueCallback);
-timer_setCallback(TIMER_3, greenCallback);
-
-timer_start(&timer0, 1000U);
-
-uint8_t blockingDemoDone = 0U;
-uint8_t oneShotDemoDone  = 0U;
-uint8_t buttonState;
-uint8_t previousState = 1U;
-
-while (1)
-{
-    gpio_digitalRead(&button, &buttonState);
-
-    if ((previousState == 1U) && (buttonState == 0U))
+    timer_configType timer0 =
     {
-        if (blockingDemoDone == 0U)
+        .number     = TIMER_0,
+        .channel    = TIMER_AB,
+        .mode       = TIMER_MODE_PERIODIC,
+        .direction  = TIMER_COUNT_DOWN,
+        .size       = TIMER_SIZE_32_BIT,
+        .prescaler  = 0U,
+        .unit       = TIMER_MS,
+        .interrupt  = TIMER_INTERRUPT_ENABLE
+    };
+
+    timer_configType timer1 =
+    {
+        .number     = TIMER_1,
+        .channel    = TIMER_AB,
+        .mode       = TIMER_MODE_PERIODIC,
+        .direction  = TIMER_COUNT_DOWN,
+        .size       = TIMER_SIZE_32_BIT,
+        .prescaler  = 0U,
+        .unit       = TIMER_MS,
+        .interrupt  = TIMER_INTERRUPT_ENABLE
+    };
+
+    timer_configType timer3 =
+    {
+        .number     = TIMER_3,
+        .channel    = TIMER_AB,
+        .mode       = TIMER_MODE_ONE_SHOT,
+        .direction  = TIMER_COUNT_DOWN,
+        .size       = TIMER_SIZE_32_BIT,
+        .prescaler  = 0U,
+        .unit       = TIMER_MS,
+        .interrupt  = TIMER_INTERRUPT_ENABLE
+    };
+    timer_configType timer4 =
+    {
+        .number     = TIMER_4,
+        .channel    = TIMER_AB,
+        .mode       = TIMER_MODE_ONE_SHOT,
+        .direction  = TIMER_COUNT_DOWN,
+        .size       = TIMER_SIZE_32_BIT,
+        .prescaler  = 0U,
+        .unit       = TIMER_MS,
+        .interrupt  = TIMER_INTERRUPT_DISABLE
+    };
+
+    /* Initialize timers and set callbacks */
+    timer_init(&timer0);
+    timer_init(&timer1);
+    timer_init(&timer3);
+    timer_init(&timer4);
+
+    timer_setCallback(TIMER_0, redCallback);
+    timer_setCallback(TIMER_1, blueCallback);
+    timer_setCallback(TIMER_3, greenCallback);
+
+    timer_start(&timer0, 1000U);
+
+    uint8_t blockingDemoDone = 0U;
+    uint8_t oneShotDemoDone  = 0U;
+    uint8_t buttonState;
+    uint8_t previousState = 1U;
+
+    while (1)
+    {
+        gpio_digitalRead(&button, &buttonState);
+
+        if ((previousState == 1U) && (buttonState == 0U))
         {
-            gpio_digitalToggle(&blueLed);
-            timer_blockingDelay(&timer4, 2000U);
-            gpio_digitalToggle(&blueLed);
+            if (blockingDemoDone == 0U)
+            {
+                gpio_digitalToggle(&blueLed);
+                timer_blockingDelay(&timer4, 2000U);
+                gpio_digitalToggle(&blueLed);
 
-            blockingDemoDone = 1U;
+                blockingDemoDone = 1U;
+            }
+            else if (oneShotDemoDone == 0U)
+            {
+                timer_start(&timer3, 4000U);
+
+                oneShotDemoDone = 1U;
+            }
+
+            /* Wait for button release */
+            do
+            {
+                gpio_digitalRead(&button, &buttonState);
+            }
+            while (buttonState == 0U);
+
+            /* Simple debounce delay */
+            timer_blockingDelay(&timer4, 20U);
         }
-        else if (oneShotDemoDone == 0U)
-        {
-            timer_start(&timer3, 4000U);
 
-            oneShotDemoDone = 1U;
-        }
-
-        /* Wait for button release */
-        do
-        {
-            gpio_digitalRead(&button, &buttonState);
-        }
-        while (buttonState == 0U);
-
-        /* Simple debounce delay */
-        timer_blockingDelay(&timer4, 20U);
+        previousState = buttonState;
     }
-
-    previousState = buttonState;
-}
 }
